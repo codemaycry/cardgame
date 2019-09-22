8f95155f168ad2f5665291e054c7b278e5b3af37
codemaycry <55370598+codemaycry@users.noreply.github.com>
2019-09-22 20:42:36 +0700

    [v1] Add more word
    add new word and make it random

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import WordCard from './WordCard';
 import styles from './App.css';
-const word = "Hello";
+const word = ['hello','whale','queen','dodge','snipe','prank']
+var randomedWord = word[Math.floor(Math.random()*word.length)]
 class App extends Component {
     render() {
         return ( < div >
-            <WordCard value = "hello"/>
+            <WordCard value = { randomedWord.toUpperCase() } />
             <h2 id="gameEnded"></h2>
             <h2 id="answer"></h2>
             </div>
