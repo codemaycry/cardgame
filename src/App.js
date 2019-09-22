7aaabe0db73b85289ec2e106e3602bbd6e4fb6d4
codemaycry <55370598+codemaycry@users.noreply.github.com>
2019-09-22 20:47:10 +0700

    Add new game button

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ import styles from './App.css';
 const word = ['hello','whale','queen','dodge','snipe','prank']
 var randomedWord = word[Math.floor(Math.random()*word.length)]
 class App extends Component {
+    restart = () =>
+    {
+      window.location.reload(false)
+    }
     render() {
         return ( < div >
             <WordCard value = { randomedWord.toUpperCase() } />
             <h2 id="gameEnded"></h2>
             <h2 id="answer"></h2>
+            <button class="Restartbutton" onClick= { this.restart }>New game</button>
             </div>
         );
     }
